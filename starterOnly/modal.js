c1d4a139ddeccdb4ff63005e4a121ff428c53d06
Alexandre SIX <alexandresix2@gmail.com>
2021-07-01 11:50:25 +0200

    realisation js bouton closeFormulaire

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -11,10 +11,15 @@ function editNav() {
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
+const closeForm = document.querySelector("main div.bground div.content span.close")//chercher le bouton(x)
+
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
-
+closeForm.addEventListener('click',function(){
+  const form=document.querySelector("main div.bground")
+  form.style.display="none";
+});
 // launch modal form
 function launchModal() {
   modalbg.style.display = "block";
