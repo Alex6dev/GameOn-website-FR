823aa512a4697c50da1f60643464fa2b9a25eab9
Alexandre SIX <alexandresix2@gmail.com>
2021-07-20 13:29:54 +0200

    projet fini commentaire fait

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -6,6 +6,7 @@ function editNav() {
     x.className = "topnav";
   }
 }
+/*---------------fonction d'affichage ---------------------------*/
 function block(border, texte){
   border.style.border="2px solid #e54858";
   let elt=texte;
@@ -37,70 +38,69 @@ const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
 
-const closeForm = document.querySelector("main div.bground div.content span.close")//chercher le bouton(x)
-const firstName = document.getElementById("first");
-const boxFirst = document.getElementsByClassName("errorFirst");
-const lastName = document.getElementById("last");
-const boxLast = document.getElementsByClassName("errorLast");
-const eMail = document.getElementById("email");
-const boxEmail = document.getElementsByClassName("errorEmail");
-const birtDate = document.getElementById("birthdate");
-const boxBirt = document.getElementsByClassName("errorBirtdate");
-const quantityTournament = document.getElementById("quantity");
-const boxQuantity = document.getElementsByClassName("errorQuantity");
-const ville= document.getElementsByClassName("checkbox-icon");
-const locaNY = document.getElementById("location1");
-const locaSF = document.getElementById("location2");
-const locaSea = document.getElementById("location3");
-const locaChi = document.getElementById("location4");
-const locaBos = document.getElementById("location5");
-const locaPor = document.getElementById("location6");
-const boxVille = document.getElementsByClassName("errorVille");
-const conditionDutilisateur = document.getElementById("checkbox1");
-const boxCondition = document.getElementsByClassName("errorCondition")
-const preventEvent = document.getElementById("checkbox2");
-const btnSendForm = document.getElementById("btn-sendForm");
-let quantityTourm = 0;
-let a= 0;
-let b= 0;
-let c= 0;
-let d= 0;
-let e= 0;
-let f= 0;
-let j= 1;
-
-
-
+const closeForm = document.querySelector("main div.bground div.content span.close")//le bouton(x)
+const firstName = document.getElementById("first");//input Firstname
+const boxFirst = document.getElementsByClassName("errorFirst");//texte erreur First
+const lastName = document.getElementById("last");//input Lastname
+const boxLast = document.getElementsByClassName("errorLast");//texte erreur Lastname
+const eMail = document.getElementById("email");//input Email
+const boxEmail = document.getElementsByClassName("errorEmail");//texte erreur Email
+const birtDate = document.getElementById("birthdate");//input Birthday 
+const boxBirt = document.getElementsByClassName("errorBirtdate");//texte erreur Birthday
+const quantityTournament = document.getElementById("quantity");//input nombre de tournoi 
+const boxQuantity = document.getElementsByClassName("errorQuantity");//texte erreur nombre de tournoi 
+const locaNY = document.getElementById("location1");//input New York
+const locaSF = document.getElementById("location2");//input San Francisco
+const locaSea = document.getElementById("location3");//input Seattle
+const locaChi = document.getElementById("location4");//input Chicago
+const locaBos = document.getElementById("location5");//input Boston
+const locaPor = document.getElementById("location6");//input Portland
+const boxVille = document.getElementsByClassName("errorVille");//texte erreur ville
+const conditionDutilisateur = document.getElementById("checkbox1");//input condition obligatoire
+const boxCondition = document.getElementsByClassName("errorCondition");//texte erreur condition obligatoire
+const preventEvent = document.getElementById("checkbox2");//input abonnement newsletter
+const btnSendForm = document.getElementById("btn-sendForm");//bouton envoi
+let quantityTourm = 0;// nombre de tournoi effectuer
+let a= 0;//variable pour envoi du formulaire
+let b= 0;//variable pour envoi du formulaire
+let c= 0;//variable pour envoi du formulaire
+let d= 0;//variable pour envoi du formulaire
+let e= 0;//variable pour envoi du formulaire
+let f= 0;//variable pour envoi du formulaire
+let j= 1;//variable pour envoi du formulaire
 
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 
-closeForm.addEventListener('click',close);
-firstName.addEventListener('input',first);
-lastName.addEventListener('input',last);
-eMail.addEventListener('input',email);
-birtDate.addEventListener('input',birt);
-quantityTournament.addEventListener('input',quantity);
-locaNY.addEventListener('input',locat);
-locaSF.addEventListener('input',locat);
-locaSea.addEventListener('input',locat);
-locaChi.addEventListener('input',locat);
-locaBos.addEventListener('input',locat);
-locaPor.addEventListener('input',locat);
-conditionDutilisateur.addEventListener('input', conditionD);
-preventEvent.addEventListener('input', conditionF);
-btnSendForm.addEventListener('click', sendForm);
+closeForm.addEventListener('click',close);//ecouter evenement pour fermer le formulaire
+firstName.addEventListener('input',first);//ecouter evenement Firstname
+lastName.addEventListener('input',last);//ecouter evenement Lastname
+eMail.addEventListener('input',email);//ecouter evenement Email
+birtDate.addEventListener('input',birt);//ecouter evenement Birthday
+quantityTournament.addEventListener('input',quantity);//ecouter evenement nombre de tournoi
+locaNY.addEventListener('input',locat);//ecouter evenement New York
+locaSF.addEventListener('input',locat);//ecouter evenement San Francisco
+locaSea.addEventListener('input',locat);//ecouter evenement Seattle
+locaChi.addEventListener('input',locat);//ecouter evenement Chicago
+locaBos.addEventListener('input',locat);//ecouter evenement Boston
+locaPor.addEventListener('input',locat);//ecouter evenement Portland
+conditionDutilisateur.addEventListener('input', conditionD);//ecouter evenement condition obligatoire
+preventEvent.addEventListener('input', conditionF);//ecouter evenement abonnement newsletter
+btnSendForm.addEventListener('click', sendForm);//ecouter evenement bouton envoi
 
 
 // launch modal form
+//---------------------ouverture du formulaire------------------------
 function launchModal() {
   modalbg.style.display = "block";
 }
+//------------------------fermeture du formulaire--------------------
 function close(){
   const form=document.querySelector("main div.bground")
   form.style.display="none";
 }
+/*----------------------------fonction Firstname--------------------------------*/
 function first(evt){
   const valFirstName= evt.target.value ;
   const regex1= new RegExp('^[a-zA-Z]{3,}')
@@ -112,7 +112,7 @@ function first(evt){
     block(firstName,boxFirst)
   }
 }
-
+/*----------------------------fonction Lastname--------------------------------*/
 function last(evt){
   const valLastName= evt.target.value ;
   const regex1= new RegExp('^[a-zA-Z]{3,}')
@@ -126,6 +126,7 @@ function last(evt){
     
   }
 }
+/*----------------------------fonction Email--------------------------------*/
 function email(evt){
   const Email= evt.target.value ;
   const regex2= new RegExp('^[a-zA-Z0-9.-_]+[@]{1}[a-zA-Z0-9.-_]+[.]{1}[a-z]{2,10}$')
@@ -137,6 +138,7 @@ function email(evt){
     block(eMail,boxEmail)
   }
 }
+/*----------------------------fonction birthday--------------------------------*/
 function birt(evt){
   const birthdate= evt.target.value ;
   const regex3= new RegExp('[0-9]')
@@ -148,6 +150,7 @@ function birt(evt){
     block(birtDate,boxBirt)
   }
 }
+/*----------------------------fonction nombre de tournoi--------------------------------*/
 function quantity(evt){
   quantityTourm= evt.target.value ;
   const regex3= new RegExp('[0-9]')
@@ -165,6 +168,7 @@ function quantity(evt){
     f=0;
   }
 }
+/*----------------------------fonction ville --------------------------------*/
 function locat(evt){
   if(evt.target.checked){
     f=1;
@@ -175,6 +179,7 @@ function locat(evt){
     blockX(boxVille);
   }
 }
+/*----------------------------fonction condition obligatoire--------------------------------*/
 function conditionD(evt){
   const conditionV= evt.target;
   if (conditionV.checked){
@@ -186,10 +191,12 @@ function conditionD(evt){
     blockX(boxCondition);
   }
 }
+/*----------------------------fonction abonnement newsletter--------------------------------*/
 function conditionF(evt){
 }
+/*----------------------------fonction envoi du formulaire --------------------------------*/
 function sendForm(evt){
-  
+  /* verifie la validiter de toutes les conditions */
   if(a!=1){
     block(firstName,boxFirst);
     evt.preventDefault();
@@ -212,13 +219,15 @@ function sendForm(evt){
     blockX(boxCondition);
     evt.preventDefault();
   }else{
+    /*si tout est bon on lance ça */
     evt.preventDefault();
     console.log("envoyer");
-    modalbg.style.display = "none";
-    const fondConfirmation=document.getElementById("fond-confirmation");
-    fondConfirmation.style.display="block";
-    const btnCloseConfirmation=document.getElementById("btn-end");
-    btnCloseConfirmation.addEventListener('click',closeConfirmation);
+    modalbg.style.display = "none";//fait disparaitre le formulaire 
+    const fondConfirmation=document.getElementById("fond-confirmation");//cherche le message de confirmation
+    fondConfirmation.style.display="block";//affiche le message de confirmation
+    const btnCloseConfirmation=document.getElementById("btn-end");//cherche le bouton fermeture du message
+    btnCloseConfirmation.addEventListener('click',closeConfirmation);//ecoute evenement du bouton fermeture
+    /* -------------------fonction fermeture du message de confirmation-----------------*/
     function closeConfirmation(){
       console.log("merci")
       fondConfirmation.style.display="none";
